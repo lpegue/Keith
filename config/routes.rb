af4d37506331319ffd8d9d6ea318cc8bb09a0637
Leslie Pegue <lpegue4@gmail.com>
2015-09-16 14:36:30 -0700

    add link user to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
  root to: "static_pages#home"
 
+ get '/home' => 'static_pages#home'
+
  get '/contact' => 'static_pages#contact'
 
  get '/about' => 'static_pages#about'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914180236) do
+ActiveRecord::Schema.define(version: 20150916212814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,10 @@ ActiveRecord::Schema.define(version: 20150914180236) do
     t.integer  "wine_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
   add_index "reviews", ["wine_id"], name: "index_reviews_on_wine_id", using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -47,5 +49,6 @@ ActiveRecord::Schema.define(version: 20150914180236) do
     t.datetime "updated_at",          null: false
   end
 
+  add_foreign_key "reviews", "users"
   add_foreign_key "reviews", "wines"
 end
